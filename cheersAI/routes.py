f0841a810f5d56def8fb9617d9f02476b418379d
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-08 09:04:17 +0545

    resolve #7

--- cheersAI/routes.py
+++ cheersAI/routes.py
@@ -5,3 +5,11 @@ from flask import render_template
 @application.route('/')
 def index():
     return render_template("dashboard.html")
+
+@application.route('/dr')
+def dr():
+    return render_template("dr.html")
+
+@application.route('/glaucoma')
+def glaucoma():
+    return render_template("glaucoma.html")

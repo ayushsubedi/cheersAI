7b1b0ef403aef802ca6f23fe2f05c4bea2896937
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-13 18:12:18 +0545

    remove old code

--- cheersAI/patients_routes.py
+++ cheersAI/patients_routes.py
@@ -6,14 +6,10 @@ from cheersAI.forms import PatientForm
 @application.route("/all_patients", methods=['GET'])
 def all_patients():
     return render_template('all_patients.html')
+    
 
 @application.route("/patient/create", methods=['GET', 'POST'])
 def patient_create():
-    if request.method == 'POST':
-        print (jsonify(request.form))
-    return render_template('create_patient.html')
-
-@application.route("/patient/create2", methods=['GET', 'POST'])
-def patient_create2():
     form = PatientForm()
-    return render_template('create_patient_wtf.html', form=form)
+    return render_template('create_patient.html', form=form)
+

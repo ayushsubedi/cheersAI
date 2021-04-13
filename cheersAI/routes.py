2dadf42adb8381a586ab843253cf68f1b4a38b5e
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-13 12:52:31 +0545

    resolve #31

--- cheersAI/routes.py
+++ cheersAI/routes.py
@@ -2,7 +2,6 @@ from cheersAI import application
 from flask import render_template, request, flash
 
 
-
 @application.route("/all_patients", methods=['GET'])
 def all_patients():
     return render_template('all_patients.html')

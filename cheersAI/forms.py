516ba64595b9fdf65682f3165e6a57aec6b0fe36
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-13 19:20:25 +0545

    resolve #35

--- cheersAI/forms.py
+++ cheersAI/forms.py
@@ -2,17 +2,16 @@ from cheersAI import application
 from cheersAI.helper import all_countries
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, NumberRange
 
-country_list = ['Nepal', 'India', 'Other South Asian Country', 'Other']
 
 class PatientForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=50)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=50)])
-    cheers_id = StringField('Cheers ID', validators=[Length(min=2, max=50)])
+    cheers_id = StringField('Cheers ID')
     address = StringField('Address', validators=[DataRequired(), Length(min=2, max=50)])
     gender = SelectField('Gender', choices=['Not Specified', 'Male', 'Female'], validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired(), Length(min=2, max=50)])
-    country = SelectField('Country', choices=all_countries(), validators=[DataRequired()])
+    age = IntegerField('Age', validators=[NumberRange(min=0, max=150)])
+    country = SelectField('Country', choices=all_countries())
     submit  = SubmitField('Add Patient')
 
--- cheersAI/patients_routes.py
+++ cheersAI/patients_routes.py
@@ -1,6 +1,8 @@
 from cheersAI import application
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, flash, redirect, url_for
 from cheersAI.forms import PatientForm
+from cheersAI.models import Patient
+from cheersAI import db
 
 
 @application.route("/all_patients", methods=['GET'])
@@ -11,5 +13,30 @@ def all_patients():
 @application.route("/patient/create", methods=['GET', 'POST'])
 def patient_create():
     form = PatientForm()
+    if form.validate_on_submit():
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        age = request.form['age']
+        gender = request.form['gender']
+        address = request.form['address']
+        country = request.form['country']
+        cheers_id = request.form['cheers_id']
+        print (request.form.items)
+        new_patient = Patient(
+            first_name=first_name, 
+            last_name=last_name, 
+            age=age, 
+            gender=gender, 
+            address=address,
+            country=country,
+            cheers_id=cheers_id)
+        try:
+            db.session.add(new_patient)
+            db.session.commit()
+            flash (f"Patient created successfully.", "success")
+            return redirect(url_for('all_patients'))
+        except Exception as e:
+            flash (f"Something went wrong."+str(e), "danger")
+            return redirect(url_for('all_patients'))
     return render_template('create_patient.html', form=form)
 

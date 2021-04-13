071ac1c064e1bdc4a169a093e8b8604abb676917
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-13 12:51:04 +0545

    reset all routes

--- cheersAI/routes.py
+++ cheersAI/routes.py
@@ -1,14 +1,9 @@
 from cheersAI import application
 from flask import render_template, request, flash
-from cheersAI import basic_auth
-from flask_uploads import IMAGES, UploadSet, configure_uploads
-from flask_uploads.exceptions import UploadNotAllowed
-from cheersAI.helper import transform_image
 
 
 
 @application.route("/all_patients", methods=['GET'])
-@basic_auth.required
 def all_patients():
     return render_template('all_patients.html')
 

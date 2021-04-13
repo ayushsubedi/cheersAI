287c784c880b9bc26316615b44bb33e9272795a3
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-13 19:36:20 +0545

    resolve #37

--- cheersAI/patients_routes.py
+++ cheersAI/patients_routes.py
@@ -14,29 +14,21 @@ def all_patients():
 def patient_create():
     form = PatientForm()
     if form.validate_on_submit():
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        age = request.form['age']
-        gender = request.form['gender']
-        address = request.form['address']
-        country = request.form['country']
-        cheers_id = request.form['cheers_id']
-        print (request.form.items)
         new_patient = Patient(
-            first_name=first_name, 
-            last_name=last_name, 
-            age=age, 
-            gender=gender, 
-            address=address,
-            country=country,
-            cheers_id=cheers_id)
+            first_name=request.form['first_name'], 
+            last_name=request.form['last_name'], 
+            age=request.form['age'], 
+            gender=request.form['gender'], 
+            address=request.form['address'],
+            country=request.form['country'],
+            cheers_id=request.form['cheers_id'])
         try:
             db.session.add(new_patient)
             db.session.commit()
             flash (f"Patient created successfully.", "success")
-            return redirect(url_for('all_patients'))
         except Exception as e:
             flash (f"Something went wrong."+str(e), "danger")
+        finally:
             return redirect(url_for('all_patients'))
     return render_template('create_patient.html', form=form)
 

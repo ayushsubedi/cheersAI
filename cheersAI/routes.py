cf1204592f19912f2ceedcd5ea694718446f686c
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-13 12:50:31 +0545

    remove previous layout

--- cheersAI/routes.py
+++ cheersAI/routes.py
@@ -5,37 +5,14 @@ from flask_uploads import IMAGES, UploadSet, configure_uploads
 from flask_uploads.exceptions import UploadNotAllowed
 from cheersAI.helper import transform_image
 
-photos = UploadSet("photos")
 
-configure_uploads(application, photos)
 
-@application.route("/dr", methods=['GET', 'POST'])
+@application.route("/all_patients", methods=['GET'])
 @basic_auth.required
-def dr():
-    filename_left, filename_right = None, None
-    if request.method == 'POST' and ('photo_left' or 'photo_right') in request.files:
-        try:
-            filename_left = photos.save(request.files['photo_left'])
-            filename_right = photos.save(request.files['photo_right'])
-            flash("Photo saved successfully.")
-        except UploadNotAllowed:
-            flash("Failed. Please select jpeg, jpg or png.")
-        content = {
-            "filename_left":filename_left, 
-            "filename_right":filename_right, 
-            "prediction_left":transform_image('cheersAI/static/uploaded_img/'+filename_left),
-            "prediction_right":transform_image('cheersAI/static/uploaded_img/'+filename_left)
-        }
-        return render_template('dr.html', **content)
-    return render_template('dr.html')
+def all_patients():
+    return render_template('all_patients.html')
 
 
 @application.route('/')
 def index():
     return render_template("dashboard.html")
-
-
-@application.route('/glaucoma')
-@basic_auth.required
-def glaucoma():
-    return render_template("glaucoma.html")

93a9b156943e5af86baf114671e1004f124018cb
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-13 13:27:27 +0545

    create patient

--- cheersAI/patients_routes.py
+++ cheersAI/patients_routes.py
@@ -5,3 +5,7 @@ from flask import render_template, request
 @application.route("/all_patients", methods=['GET'])
 def all_patients():
     return render_template('all_patients.html')
+
+@application.route("/create_patient", methods=['GET'])
+def create_patient():
+    return render_template('create_patient.html')

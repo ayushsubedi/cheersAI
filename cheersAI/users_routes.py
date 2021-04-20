f7b8b0aa262aab50960ca956bfb4759c464e8230
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-20 10:37:49 +0545

    login poc

--- cheersAI/users_routes.py
+++ cheersAI/users_routes.py
@@ -1,35 +1,11 @@
 from cheersAI import application
-from flask import render_template, request
+from flask import render_template, request, jsonify, flash, redirect, url_for
 from cheersAI.forms import LoginForm
+
 
 @application.route("/all_users", methods=['GET'])
 def all_users():
     return render_template('all_users.html')
-
-# @application.route("/patient/create", methods=['GET', 'POST'])
-# def patient_create():
-#     form = PatientForm()
-#     if form.validate_on_submit():
-#         new_patient = Patient(
-#             first_name=request.form['first_name'], 
-#             last_name=request.form['last_name'], 
-#             age=request.form['age'], 
-#             email=request.form['email'],
-#             phone=request.form['phone'],
-#             gender=request.form['gender'], 
-#             address=request.form['address'],
-#             country=request.form['country'],
-#             cheers_id=request.form['cheers_id'])
-#         try:
-#             db.session.add(new_patient)
-#             db.session.commit()
-#             flash (f"Patient created successfully.", "success")
-#             return redirect(url_for('patient', patient_id=new_patient.id))
-#         except Exception as e:
-#             flash (f"Something went wrong."+str(e), "danger")
-#             return redirect(url_for('all_patients'))    
-#     return render_template('create_patient.html', title="Create New Patient", action='patient_create', form=form)
-
 
 
 @application.route("/login", methods=['GET', 'POST'])
@@ -38,7 +14,7 @@ def login():
     if form.validate_on_submit():
         email = request.form['email']
         password = request.form['password']
-        print (email, password)
-        return render_template('login.html', form=form)
+        flash (f"Login Successful.", "success")
+        return redirect(url_for('index'))
     return render_template('login.html', form=form)
 

292489aa6d5318d3b774b50d1701d38299c6f713
Ayush Subedi <ayush.subedi@gmail.com>
2021-04-30 14:59:15 +0545

    resolve #138

--- cheersAI/routes.py
+++ cheersAI/routes.py
@@ -2,10 +2,6 @@ from cheersAI import application
 from flask import render_template, request
 from cheersAI.helper import login_required
 
-@application.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template("dashboard.html")
 
 @application.route('/')
 def index():
